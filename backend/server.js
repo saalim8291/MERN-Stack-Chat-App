cd01c7bc827de8f1add1053344a14d0a940cc6a5
Saalim Bhatkar <Saalim.Bhatkar@teknopoint.local>
2022-03-26 22:19:16 +0530

    server connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const { chats } = require('./data/data');
 const app = express();
-const PORT = 8000;
+
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+app.get('/api/chat/', (req, res) => {
+    res.send(chats)
+    // const singleChat = chats.find(chat => chat._id===req.params.id);
+    res.send(singleChat)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    // res.send(chats)
+    const singleChat = chats.find(chat => chat._id===req.params.id);
+    res.send(singleChat)
+})
 
 app.listen(PORT, () => {
-    console.log('port is running')
-})
+    console.log(`port is running on ${PORT}`)
+});
