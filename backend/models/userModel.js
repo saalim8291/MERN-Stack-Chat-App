f973a4146682392d068c246212f5a46feacbd7a6
Saalim Bhatkar <Saalim.Bhatkar@teknopoint.local>
2022-03-27 22:02:30 +0530

    user route and user controller backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const userModel = mongoose.Schema({
-    name: {type: String, required},
-    email: {type: String, required},
-    password: {type: String, required},
-    picture: {type: String, required, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}
+    name: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    picture: {type: String, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}
 }, {timestamps: true});
 
 const User = mongoose.model("User", userModel);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const { chats } = require('./data/data');
 const app = express();
-
 const dotenv = require('dotenv');
 dotenv.config();
+
+const userRoutes = require('./routes/userRoutes');
+
+
+const connectDB = require('./config/db');
+connectDB();
+
+
 const PORT = process.env.PORT || 8000;
 
-app.get('/api/chat/', (req, res) => {
-    res.send(chats)
-    // const singleChat = chats.find(chat => chat._id===req.params.id);
-    // res.send(singleChat)
-})
+app.use(express.json());
+app.use('/api/user/', userRoutes);
 
-app.get('/api/chat/:id', (req, res) => {
-    // res.send(chats)
-    const singleChat = chats.find(chat => chat._id===req.params.id);
-    res.send(singleChat)
-})
+// app.get('/api/chat/:id', (req, res) => {
+//     // res.send(chats)
+//     const singleChat = chats.find(chat => chat._id===req.params.id);
+//     res.send(singleChat)
+// })
 
 app.listen(PORT, () => {
     console.log(`port is running on ${PORT}`)
